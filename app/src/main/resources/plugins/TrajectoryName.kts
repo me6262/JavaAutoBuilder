6f89f030208a71ddf671a66b0052857589a709c0
me6262 <34924631+me6262@users.noreply.github.com>
2024-02-26 11:06:41 -0500

    edited the trajectory name plugin to work on windows better fixes #14

--- app/src/main/resources/plugins/TrajectoryName.kts
+++ app/src/main/resources/plugins/TrajectoryName.kts
@@ -1,17 +1,16 @@
 import java.io.File
 import java.util.ArrayList
-import java.util.regex.Pattern
 import frc238.App
 import frc238.background.RobotProject.Constants
+import org.jetbrains.kotlin.util.removeSuffixIfPresent
 
 // gets the list of trajectories
 val trajectories = ArrayList<String>()
-val pattern = Pattern.compile((App.project.rootDirectory + Constants.deployDirectory) + "choreo${File.separatorChar}")
 val dir = File(App.project.rootDirectory + Constants.deployDirectory + "choreo${File.separatorChar}")
-val dirList = dir.listFiles()!!
+val dirList: Array<File> = dir.listFiles()!!
 for (child in dirList) {
     println(child.name)
-    trajectories!!.add(child.name.split("\\.traj".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0])
+    trajectories.add(child.name.removeSuffixIfPresent(".traj"))
 }
 
 //technically, this is an expression, which means awful things that i absolutely hate
